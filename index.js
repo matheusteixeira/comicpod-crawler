209a684705bcd20480f11998d4869affd3f5b3b3
matheusteixeira <matheusteixeira01@live.com>
2018-10-17 10:01:06 -0300

    Loop through all pages to crawl titles

--- index.js
+++ index.js
@@ -2,67 +2,70 @@ var request = require('request')
 var cheerio = require('cheerio')
 var fs = require('fs')
 
-request('http://www.terrazero.com.br/category/comicpod-podcast-quadrinhos/', function(err, res, body) {
-    if (err) console.log(`Erro: ${err}`)
+var pages = []
 
-    var $ = cheerio.load(body)
+for(var i = 1; i <= 23; i++) {
+    pages.push('http://www.terrazero.com.br/category/comicpod-podcast-quadrinhos/page/' + i);
+}
 
-    var arrayResponse = []
+console.log(pages);
 
-    var objPodcast = {}
-
-    $('.herald-posts article').each(function() {
-
-        var postPodcast = $(this).find('a').attr('href')
-
-        request(postPodcast, function(err, res, body) {
+pages.forEach(function(url){
+    request(url, function(err, res, body) {
             if (err) console.log(`Erro: ${err}`)
 
             var $ = cheerio.load(body)
 
-            var tituloPodcast = $('.entry-title').first().text()
-            
-            var descPodcast = $('.entry-content p').first().text()
-            
-            var linkPodcast = $('.powerpress_links_mp3').first().find("a").first().attr('href')
+            var arrayResponse = []
 
-            objPodcast = {
-                "titulo": tituloPodcast,
-                "descricao": descPodcast,
-                "link": linkPodcast
-            }
-            
-            arrayResponse.push(objPodcast)
+            var objPodcast = {}
+
+            $('.herald-posts article').each(function() {
+
+                var postPodcast = $(this).find('a').attr('href')
+
+                request(postPodcast, function(err, res, body) {
+                    if (err) console.log(`Erro: ${err}`)
+                    var $ = cheerio.load(body)
+
+                    var tituloPodcast = $('.entry-title').first().text()
+                    console.log(tituloPodcast)
+                    var descPodcast = $('.entry-content p').first().text()
+
+                    var linkPodcast = $('.powerpress_links_mp3').first().find("a").first().attr('href')
+
+                    objPodcast = {
+                        "titulo": tituloPodcast,
+                        "descricao": descPodcast,
+                        "link": linkPodcast
+                    }
+
+                    arrayResponse.push(objPodcast)
+
+                })
+
+                // console.log(arrayResponse);
+
+            })
+        // fs.appendFile('podcasts.json', JSON.stringify(arrayResponse))
+
+        // $('.herald-posts article').each(function(){
+
+        //     var postPodcast = $(this).find('a').attr('href')
+
+        //     request(postPodcast, function(err, res, body) {
+        //         if (err) console.log(`Erro: ${err}`)
+
+        //         var $ = cheerio.load(body)
+
+        //         var tituloPodcast = $('.entry-title').first().text()
+        //         var tituloPodcast = $('.entry-title').first().text()
+
+        //         console.log(`Titulo: ${tituloPodcast}`)
+
+        //     })
+
+        //     //
 
         })
-
-        console.log(arrayResponse);
-
-    })
-
-    // fs.appendFile('podcasts.json', JSON.stringify(arrayResponse))
-
-    // $('.herald-posts article').each(function(){
-
-    //     var postPodcast = $(this).find('a').attr('href')
-
-    //     request(postPodcast, function(err, res, body) {
-    //         if (err) console.log(`Erro: ${err}`)
-
-    //         var $ = cheerio.load(body)
-
-    //         var tituloPodcast = $('.entry-title').first().text()
-            
-    //         var tituloPodcast = $('.entry-title').first().text()
-
-    //         console.log(`Titulo: ${tituloPodcast}`)
-
-    //     })
-
-    //     // 
-
-
-
-
-
-})
+});
